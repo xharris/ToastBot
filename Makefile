29bbca30f4a8cef64ae284c96c0b55e450bbe685
Xavier Harris <xharris1@umbc.edu>
2018-01-15 14:46:19 -0500

    first commit

---
+++ Makefile
@@ -0,0 +1,5 @@
+run:
+	py main.py
+
+setup:
+	py -m pip install -U discord.py[voice]
---
+++ main.py
@@ -0,0 +1,8 @@
+# 🎶
+
+from musicbot import MusicBot
+import re
+if __name__ == "__main__":
+	bot_instance = MusicBot()
+	bot_instance.run('MjgzMDU4NzEzNzQ3NzgzNjgw.DT1_5A.DPq9LhvyI7OExHEVjmsxbLVj0so')
+
